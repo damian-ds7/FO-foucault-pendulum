245dde47f2435c9afa4a0a43af2bb6afba0fd7ef
Michał Szwejk <mszwejk1@gmail.com>
2025-11-20 19:37:21 +0100

    vectorize calculations

--- sketch.js
+++ sketch.js
@@ -17,13 +17,41 @@ let dt = 0.001;
 let maxTracePoints = 10_000;
 let scale = 2;
 
+class Vector {
+    constructor(components) {
+        this.components = components;
+        this.dimension = components.length;
+    }
+}
+
+
+function scaleVector(vector, scalar) {
+    return new Vector(vector.components.map(c => c * scalar));
+}
+
+function addVectors(...vectors) {
+    const dimension = vectors[0].dimension;
+    const result = new Array(dimension).fill(0);
+    for (const vec of vectors) {
+        for (let i = 0; i < dimension; i++) {
+            result[i] += vec.components[i];
+        }
+    }
+    return new Vector(result);
+}
 
 class Pendulum {
     constructor() {
-        this.x = initialAmplitude;
-        this.y = 0;
-        this.vx = 0;
-        this.vy = 0;
+        this.state = new Vector([initialAmplitude, 0, 0, 0]);
+        this.trace = [];
+    }
+
+    x() {
+        return this.state.components[0];
+    }
+
+    y() {
+        return this.state.components[1];
     }
 
     a_x(x, vy) {
@@ -34,37 +62,24 @@ class Pendulum {
         return - (2 * vx * earthOmega * Math.sin(latitude)) - (g / pendulumLength) * y;
     }
 
+    derivative(state) {
+        const [x, y, vx, vy] = state.components;
+        return new Vector([vx, vy, this.a_x(x, vy), this.a_y(y, vx)])
+    }
+
 
     runge_kutt(h) {
-        const x = this.x;
-        const y = this.y;
-        const vx = this.vx;
-        const vy = this.vy;
+        const s = this.state;
 
-        const k1_x = h * vx;
-        const k1_y = h * vy;
-        const k1_vx = h * this.a_x(x, vy);
-        const k1_vy = h * this.a_y(y, vx);
+        const k1 = scaleVector(this.derivative(s), h);
+        const k2 = scaleVector(this.derivative(addVectors(s, scaleVector(k1, 1 / 2))), h);
+        const k3 = scaleVector(this.derivative(addVectors(s, scaleVector(k2, 1 / 2))), h);
+        const k4 = scaleVector(this.derivative(addVectors(s, k3)), h)
 
-        const k2_x = h * (vx + k1_vx / 2);
-        const k2_y = h * (vy + k1_vy / 2);
-        const k2_vx = h * this.a_x(x + k1_x / 2, vy + k1_vy / 2);
-        const k2_vy = h * this.a_y(y + k1_y / 2, vx + k1_vx / 2);
 
-        const k3_x = h * (vx + k2_vx / 2);
-        const k3_y = h * (vy + k2_vy / 2);
-        const k3_vx = h * this.a_x(x + k2_x / 2, vy + k2_vy / 2);
-        const k3_vy = h * this.a_y(y + k2_y / 2, vx + k2_vx / 2);
+        const increment = addVectors(k1, scaleVector(k2, 2), scaleVector(k3, 2), k4);
+        this.state = addVectors(this.state, scaleVector(increment, 1 / 6));
 
-        const k4_x = h * (vx + k3_vx);
-        const k4_y = h * (vy + k3_vy);
-        const k4_vx = h * this.a_x(x + k3_x, vy + k3_vy);
-        const k4_vy = h * this.a_y(y + k3_y, vx + k3_vx);
-
-        this.x += (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6;
-        this.y += (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6;
-        this.vx += (k1_vx + 2 * k2_vx + 2 * k3_vx + k4_vx) / 6;
-        this.vy += (k1_vy + 2 * k2_vy + 2 * k3_vy + k4_vy) / 6;
     }
 }
 
@@ -136,11 +151,11 @@ function drawPendulum(pendulum) {
 
     stroke(50);
     strokeWeight(2);
-    line(0, 0, pendulum.x * scale, pendulum.y * scale);
+    line(0, 0, pendulum.x() * scale, pendulum.y() * scale);
 
     fill(255, 0, 0);
     noStroke();
-    circle(pendulum.x * scale, pendulum.y * scale, 20);
+    circle(pendulum.x() * scale, pendulum.y() * scale, 20);
 
     fill(50);
     circle(0, 0, 10);
@@ -155,16 +170,13 @@ function updatePhysics() {
             if (pendulum.trace.length === maxTracePoints) {
                 pendulum.trace.shift();
             }
-            pendulum.trace.push([pendulum.x, pendulum.y]);
+            pendulum.trace.push([pendulum.x(), pendulum.y()]);
         }
     }
 }
 
 function resetPendulum() {
-    pendulum.x = initialAmplitude;
-    pendulum.y = 0;
-    pendulum.vx = 0;
-    pendulum.vy = 0;
+    pendulum.state = new Vector([initialAmplitude, 0, 0, 0])
     pendulum.trace = [];
 }
 
@@ -184,16 +196,17 @@ function setupControls() {
         select('#amplitude-val').html(initialAmplitude);
     });
 
-    select('#period').input(function() {
-        earthPeriod = this.value();
-        earthOmega = 2 * Math.PI / (earthPeriod * 60);
-        select('#period-val').html(earthPeriod + ' min');
-    });
 
     select('#damping').input(function() {
         let dampVal = this.value();
         damping = dampVal / 1000;
         select('#damping-val').html(damping.toFixed(3));
+    });
+
+    select('#period').input(function() {
+        earthPeriod = this.value();
+        earthOmega = 2 * Math.PI / (earthPeriod * 60);
+        select('#period-val').html(earthPeriod + ' min');
     });
 
     select('#speed').input(function() {
